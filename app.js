d358f84c0d484fee15aa875fd675d51a820b4913
andrews2 <andrewjshipman1@gmail.com>
2022-02-06 18:40:53 -0500

    fixed test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var dropbox = require("dropbox");
 app.use(express.json());
 dbx = new dropbox.Dropbox({accessToken: process.env.DROPBOX_KEY});
 
-filename = 'in.txt'
+filename = '/in.txt'
 dbx.filesUpload({path: filename});
 
 class User{
