1e097a8db946c9f49c927aa8db25bd9ba094c0e9
andrews2 <andrewjshipman1@gmail.com>
2022-02-06 23:08:56 -0500

    downloading files

--- app.js
+++ app.js
@@ -17,9 +17,8 @@ class User{
     this.accountValue = 0;
   }
 
-  JSONToObject(obj){
-    Object.assign(this, obj);
-    return this;
+  copyJSON(obj){
+      Object.assign(this, obj)
   }
 
   get Password(){
@@ -62,7 +61,9 @@ function loadAccountsFromDB(){
         const data = JSON.parse(response.result.fileBinary);
         const keys = Object.keys(data);
         for(let i = 0; i < keys.length; i++){
-            accounts.set(keys[i], User.JSONToObject(data[keys[i]]));
+            user = new User(data[keys[i]].username, data[keys[i]].password)
+            user.copyJSON(data[keys[i]]);
+            accounts.set(keys[i], data[keys[i]]);
         }
         console.log(accounts.get("test").Password);
     })
