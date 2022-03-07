2515b2ed88233422d92023c4d125acaf8126b112
andrews2 <andrewjshipman1@gmail.com>
2022-03-07 01:03:43 -0500

    make history file on signup

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ app.post("/login", function(req, res){
 
 app.post("/getHistGames", function(req, res){
   getGamesFile(req.body.name);
-  var options = {root: __dirname + "/histFiles"};
+  var options = {root: __dirname};
   var fileName = req.body.name + "_games.ser";
   res.sendFile(fileName, options, function(err){
     if (err) console.log(err);
@@ -199,7 +199,7 @@ app.post("/getHistGames", function(req, res){
 
 app.post("/getHistVals", function(req, res){
   getValsFile(req.body.name);
-  var options = {root: __dirname + "/histFiles"};
+  var options = {root: __dirname};
   var fileName = req.body.name + "_vals.ser";
   res.sendFile(fileName, options, function(err){
     if (err) console.log(err);
